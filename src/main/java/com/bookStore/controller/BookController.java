02c94a570d99552e36702dcaac0f5faefa0e9f94
eozkanch <chofficelch@gmail.com>
2023-02-05 23:35:54 +0100

    templatesdatabase

---
+++ src/main/java/com/bookStore/controller/BookController.java
@@ -0,0 +1,18 @@
+package com.bookStore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class BookController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+    @GetMapping("/book_Register")
+    public String bookRegister(){
+        return "bookRegister";
+    }
+
+}
